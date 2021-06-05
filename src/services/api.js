b2674093a42745e8b1265b4eff73716bf3c9e736
Andre Luiz Branco Barbosa Araujo <andreluizpsn975@hotmail.com>
2021-06-05 11:23:25 -0300

    url da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-54-158-81-235.compute-1.amazonaws.com:3333',
+  baseURL: 'http://ec2-3-80-207-31.compute-1.amazonaws.com:3333',
 });
 
 const getIp = axios.create({
